6b1d7d4fb0055164744ec61c039d006a1d5f54f9
Zorana Curkovic <zorana.curkovic@unity3d.com>
2020-10-04 13:22:44 +0100

    Project init, basic render of UVs for selected object in scene with Handles

---
+++ Assets/Editor/UVEditorWindow.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System;
+using System.Runtime.Remoting.Channels;
+using Object = UnityEngine.Object;
+
+public class UVEditorWindow : EditorWindow
+{
+    string selectedObjectText = "Selected Object";
+    string selectedMeshText = "Selected Mesh";
+
+    private GameObject m_GO;
+    private Mesh m_Mesh;
+    private int m_vertexCount;
+    private Vector2[] m_UV1;
+    private Vector2 rendererOffset = new Vector2(250,300);
+    private Vector2 rendererScale = new Vector2(100, 100);
+    private Vector2 rendererPointSize = new Vector2(3,3);
+    private UVMeshRenderer m_uvMeshRenderer;
+    
+    //private GUIStyle _boxStyle;
+    [MenuItem("Window/Rendering/UVEditor")]
+    static void Init()
+    {
+        UVEditorWindow window = (UVEditorWindow) EditorWindow.GetWindow(typeof(UVEditorWindow));
+        window.Show();
+    }
+    
+    public void OnFocus()
+    {
+        OnSelectionChange();
+    }
+
+    public void OnSelectionChange()
+    {
+        Object activeObject = Selection.activeObject;
+        GameObject activeGameObject = activeObject as GameObject;
+        MeshFilter activeMesh = activeObject as MeshFilter;
+
+        if (activeGameObject != null)
+        {
+            m_GO = activeGameObject;
+
+            MeshFilter filter = m_GO.GetComponent<MeshFilter>();
+            m_Mesh = null;
+            
+            if (filter != null)
+            {
+                m_Mesh = filter.sharedMesh;
+
+                m_vertexCount = m_Mesh.vertexCount;
+                m_UV1 = m_Mesh.uv;
+                selectedMeshText = "#" + m_vertexCount.ToString() + "=";
+                for (int i = 0; i < m_vertexCount; ++i)
+                    selectedMeshText += "["+m_UV1[i].x + "," + m_UV1[i].y+"] ";
+
+                if (m_uvMeshRenderer != null)
+                {
+                    m_uvMeshRenderer.SetPoints(m_UV1);
+                }
+                else
+                {
+                    rendererScale = new Vector2(100, 100);
+                    rendererOffset = new Vector2(400, 300);
+
+                    m_uvMeshRenderer = new UVMeshRenderer(rendererScale, rendererOffset, rendererPointSize);
+                    m_uvMeshRenderer.SetPoints(m_UV1);
+                    m_uvMeshRenderer.GenerateEdges(m_Mesh.triangles);
+                }
+            }
+            else
+            {
+                if (m_uvMeshRenderer != null)
+                {
+                    m_uvMeshRenderer.Clear();
+                    m_uvMeshRenderer = null;
+                }
+            }
+            
+        }
+
+
+        Repaint();
+    }
+
+    //private float a;
+    void OnGUI()
+    {
+        //a += Input.mouseScrollDelta.y;
+            
+        if (m_GO != null) 
+            selectedObjectText = m_GO.name.ToString();
+        else
+            selectedObjectText = "No Selected Object";
+
+        if (m_Mesh == null)
+        {
+            selectedMeshText = "No Mesh";
+        }
+ 
+
+        EditorGUILayout.Space(20);
+        EditorGUILayout.LabelField("Selected Object", selectedObjectText,EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Selected Mesh", selectedMeshText);
+
+        //_boxStyle = new GUIStyle();
+        
+        // Constrain all drawing to be within a 800x600 pixel area centered on the screen.
+       GUI.BeginGroup(new Rect(Screen.width / 2 - 400, Screen.height / 2 - 300, 800, 600));
+
+        // Draw a box in the new coordinate space defined by the BeginGroup.
+        // Notice how (0,0) has now been moved on-screen
+       // GUI.Box(new Rect(0, 0, 800, 600), "This box is now centered! - here you would put your main menu");
+
+        // We need to match all BeginGroup calls with an EndGroup
+       
+        if (m_uvMeshRenderer != null)
+        {
+            m_uvMeshRenderer.Offset = new Vector2(400, 300);
+            m_uvMeshRenderer.DrawUVMesh();
+        }
+        
+        GUI.EndGroup();
+    }
+}
---
+++ Assets/Editor/UVMeshRenderer.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class UVMeshRenderer
+{
+    private Mesh m_UVsMesh;
+
+    private static Material s_UVMaterial;
+    //private List<Vector3> points;
+    //private Matrix4x4 transform;
+    private Vector2[] m_vertices;
+    private Vector2 m_scale;
+    private Vector2 m_offset;
+    private Vector2 m_pointSize;
+    private List<List<int>> m_edges;
+
+
+
+    public UVMeshRenderer(Vector2 mScale, Vector2 mOffset, Vector2 mPointSize)
+    {
+        m_scale = mScale;
+        m_offset = mOffset;
+        m_pointSize = mPointSize;
+        
+        //Vector2 m_Translation = new Vector2(0, 0);
+        //Vector2 m_Scale = new Vector2(1, -1);
+        //transform = Matrix4x4.TRS(m_Translation, Quaternion.identity, new Vector3(m_Scale.x, m_Scale.y, 1));
+        //points = new List<Vector3>();
+    }
+
+    public void Clear()
+    {
+        m_vertices = null;
+        //points.Clear();
+    }
+
+    public Vector2 Offset
+    {
+        get => m_offset;
+        set => m_offset = new Vector2(value.x - m_scale.x / 2, value.y - m_scale.y / 2);
+    }
+
+    public Vector2 Scale
+    {
+        get => m_scale;
+        set => m_scale = value;
+    }
+
+    public void SetPoints(Vector2[] UVs)
+    {
+        m_vertices = UVs;
+        
+        for (int i = 0; i < m_vertices.Length; ++i)
+        {
+            m_vertices[i] = m_offset + (m_vertices[i] * m_scale);
+        }
+    }
+
+    public void GenerateEdges(int[] triangles)
+    {
+        Debug.Log("Set Triangles");
+        
+        m_edges = new List<List<int>>();
+        
+        for (int i = 0; i < m_vertices.Length; ++i)
+        {
+            m_edges.Add(new List<int>());
+        }
+
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            int v1 = triangles[i];
+            int v2 = triangles[i + 1];
+            int v3 = triangles[i + 2];
+
+            if (v1 < v2)
+            {
+                if (!m_edges[v1].Contains(v2))
+                    m_edges[v1].Add(v2);
+            }
+            else
+            {
+                if(!m_edges[v2].Contains(v1))
+                    m_edges[v2].Add(v1);
+            }
+            
+            if (v1 < v3)
+            {
+                if (!m_edges[v1].Contains(v3))
+                    m_edges[v1].Add(v3);
+            }
+            else
+            {
+                if(!m_edges[v3].Contains(v1))
+                    m_edges[v3].Add(v1);
+            }
+
+            if (v2 < v3)
+            {
+                if (!m_edges[v2].Contains(v3))
+                    m_edges[v2].Add(v3);
+            }
+            else
+            {
+                if (!m_edges[v3].Contains(v2))
+                    m_edges[v3].Add(v2);
+            }
+            
+        }
+
+        //PrintConnections();
+    }
+
+    private void PrintConnections()
+    {
+        for (int i = 0; i < m_edges.Count; ++i)
+        {
+            string connections = "";
+            
+            for (int j = 0; j < m_edges[i].Count; ++j)
+                connections += $" {m_edges[i][j]} ";
+            
+            Debug.Log($"Vertex {i} : [{connections}]");
+        }
+    }
+    
+
+    public void DrawUVMesh()
+    {
+        if (m_vertices == null)
+        {
+            //Debug.Log("Points 2D are null");
+            return;
+        }
+
+        
+        /* Draw edges and vertices in one go */
+
+        for (int i = 0; i < m_edges.Count; ++i)
+        {
+            Handles.color = Color.yellow;
+
+            Handles.DrawLine(m_vertices[i] - m_pointSize, m_vertices[i] + new Vector2(m_pointSize.x,-m_pointSize.y));
+            Handles.DrawLine(m_vertices[i] - m_pointSize, m_vertices[i] + new Vector2(-m_pointSize.x, m_pointSize.y));
+            Handles.DrawLine(m_vertices[i] + new Vector2(-m_pointSize.x, m_pointSize.y), m_vertices[i] + m_pointSize);
+            Handles.DrawLine(m_vertices[i] + new Vector2(m_pointSize.x,-m_pointSize.y), m_vertices[i] + m_pointSize);
+
+            Handles.color = Color.green;
+
+            for (int j = 0; j < m_edges[i].Count; ++j)
+            { 
+                Handles.DrawLine(m_vertices[i], m_vertices[m_edges[i][j]]);
+            }
+        }
+        
+    }
+    
+
+    // private void BuildCurveMesh()
+    // {
+    //     if (m_UVsMesh != null || points.Count == 0)
+    //         return;
+    //
+    //     Vector3[] vertices = points.ToArray();
+    //
+    //     m_UVsMesh = new Mesh();
+    //     m_UVsMesh.name = "UVMesh";
+    //     m_UVsMesh.hideFlags |= HideFlags.DontSave;
+    //     m_UVsMesh.vertices = vertices;
+    //
+    //     if (vertices.Length > 0)
+    //     {
+    //         int nIndices = vertices.Length - 1;
+    //         int index = 0;
+    //
+    //         List<int> indices = new List<int>(nIndices * 2);
+    //         while (index < nIndices)
+    //         {
+    //             indices.Add(index);
+    //             indices.Add(++index);
+    //         }
+    //
+    //         m_UVsMesh.SetIndices(indices.ToArray(), MeshTopology.Lines, 0);
+    //     }
+        
+        // Add to DrawMesh
+        // if (m_UVsMesh == null)
+        //    BuildCurveMesh();
+        
+        // Try rendering mesh at some point
+        //Camera.SetupCurrent(null);
+        // Material mat = uvMaterial;
+        // mat.SetColor("_Color", Color.yellow);
+        //
+        // mat.SetPass(0);
+        // Graphics.DrawMeshNow(m_UVsMesh, Handles.matrix * transform);
+    //}
+
+
+    // public static Material uvMaterial
+    // {
+    //     get
+    //     {
+    //         if (!s_UVMaterial)
+    //         {
+    //             Shader shader = (Shader)EditorGUIUtility.LoadRequired("Editors/AnimationWindow/Curve.shader");
+    //             s_UVMaterial = new Material(shader);
+    //             s_UVMaterial.hideFlags = HideFlags.HideAndDontSave;
+    //         }
+    //
+    //         return s_UVMaterial;
+    //     }
+    // }
+}
